2df47d3367c8f049ec89580b33631712e7344fb0
prabinrijal03 <prabinrijal03@gmail.com>
2024-02-26 13:26:09 +0545

    done

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -2,19 +2,34 @@ import User from "../model/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 export const register = async (req,res)=>{
+    
     try {
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(req.body.password, salt);
 
+        const usernameExists = await User.findOne({username: req.body.username});
+        if(usernameExists){
+            return res.status(500).json({existed: true, message: "username already exists"});
+        }
+        const emailExists = await User.findOne({email:req.body.email});
+        if(emailExists){
+            return res.status(500).json({existed: true, message:"email already exists"});
+        }
+        
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
             password: hash
         });
-        await newUser.save();
-        res.status(200).json("User has been created");
+       
+            await newUser.save();
+            res.status(200).json({ success: true, message:"User has been created"});
+        
+
     } catch (error) {
-        res.status(500).json(error);
+        res.status(200).json({ success: false, message:error});
+
+   
     }
 };
 export const login = async (req,res)=>{
@@ -25,17 +40,21 @@ export const login = async (req,res)=>{
             return res.status(500).json("Invalid username or email");
         }
         const user = loginByUsername || loginByEmail;
+        console.log(user.password);
+        
         const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
+      
         if(!isPasswordValid){
             return res.status(500).json("Invalid password");
         }
-
+   
         const token = jwt.sign({userId: user._id, isAdmin: user.isAdmin}, 'secretKey');
 
         const {password, isAdmin, ...otherDetails} = user._doc;
         res.cookie("access_token", token, {httpOnly:true}).status(200).json({...otherDetails});
 
     } catch (error) {
-        res.status(500).json(error);
+        return res.status(500).json("Something went wrong");
+     
     }
 };
